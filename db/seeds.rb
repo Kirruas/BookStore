e5050f46def72080928788a6e581298932b58253
Kirruas <jean.justine@aol.com>
2023-12-15 16:26:39 +0100

    Book added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+
+Book.destroy_all
+Book.reset_pk_sequence
+
+Book.create(
+    [
+    {
+        title: 'La Chute',
+        year: 1956
+    },
+    {
+        title: 'La Metamorphose',
+        year: 1912
+    },
+    {
+        title: 'Gargantua',
+        year: 1534
+    },
+    {
+        title: "L'Etranger",
+        year: 1942
+    },
+    {
+        title: 'Le Proces',
+        year: 1914
+    }
+    ]
+
+)
